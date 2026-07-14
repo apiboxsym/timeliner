6eebfdefb3b8f1ec74ef56e2ddd736dadf639570
k.anisimov <k.anisimov@epicentrk.ua>
2026-07-15 01:58:16 +0300

    main

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const gitlabPagesBase =
+  process.env.CI && process.env.CI_PROJECT_NAME ? `/${process.env.CI_PROJECT_NAME}/` : '/'
+
 export default defineConfig({
+  base: gitlabPagesBase,
   plugins: [react()],
 })
